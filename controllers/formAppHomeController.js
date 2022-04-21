ad065ab08596830b85acde4d2f7e5e4e4e6a303f
manikv12 <manik.vashith@nlsnow.com>
2022-04-21 09:09:26 -0500

    Initail Commit

---
+++ controllers/formAppHomeController.js
@@ -0,0 +1,22 @@
+
+
+// Functions for the routes
+
+// ----------------Hompage----------------
+// Homepage
+const formAppController_home = (req, res) => {
+    res.render('index');
+}
+// Hompage Post request
+const formAppController_homePost = (req, res) => {
+    console.log(req.body);
+    res.render('index');
+}
+
+
+
+
+module.exports = {
+    formAppController_home,
+    formAppController_homePost
+}
---
+++ routes/formApp.js
@@ -0,0 +1,12 @@
+// route module.
+
+var express = require('express');
+var router = express.Router();
+
+var formAppController = require('../controllers/formAppHomeController')
+
+// Home page route.
+router.get('/', formAppController.formAppController_home)
+router.post('/', formAppController.formAppController_homePost)
+
+module.exports = router;
